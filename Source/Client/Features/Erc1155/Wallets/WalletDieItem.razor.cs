110dd55204019bb905f52cf374f37ed3073e595a
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-10-20 18:31:56 -0400

    wallet die item style

---
+++ Source/Client/Features/Erc1155/Wallets/WalletDieItem.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace MintingUi.Client.Features.Erc1155.Wallets
+{
+  using Microsoft.AspNetCore.Components;
+  using MintingUi.Client.Features.Base.Components;
+  using MintingUi.Client.Features.Erc1155.Die;
+
+  public class WalletDieItemBase : BaseComponent
+  {
+    [Parameter] public Die Die { get; set; }
+    [Parameter] public string DieId { get; set; }
+
+    public string RedeemTokenRoute = "/WalletId/dies/{CurrencyId}/{DieId}";
+  }
+}
